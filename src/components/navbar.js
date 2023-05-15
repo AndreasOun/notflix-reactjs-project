f70f917a65266c5d958532a8854a422f9fa16e0a
AndreasOun <andreastthk@gmail.com>
2023-05-15 07:37:29 +0300

    Set up react

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV Shows</a></li>
+        <li><a href="#">My List</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
