b5a1b20f3287bc78133e2450a44c0612a52a6fda
AndreasOun <andreastthk@gmail.com>
2023-05-20 17:35:39 +0300

    fixed layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,19 +4,23 @@ import './home.css';
 
 function Home() {
   const [movies, setMovies] = useState([]);
+  const [backgroundImage, setBackgroundImage] = useState('');
 
   useEffect(() => {
     async function fetchData() {
       const API_KEY = '8a4ddcf472e26bea20a3ea9f42810899';
       const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
       setMovies(response.data.results);
+      if (response.data.results.length > 0) {
+        setBackgroundImage(`https://image.tmdb.org/t/p/original/${response.data.results[0].backdrop_path}`);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <div className="container">
-      <div className="hero">
+      <div className="hero" style={{ backgroundImage: `url(${backgroundImage})` }}>
         <h1>Welcome to Notlfix!</h1>
         <p>Here you can find your favorite movies and TV shows.</p>
       </div>
