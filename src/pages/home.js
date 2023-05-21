705b9d90b12fca231a292be33bf112d93e4b7100
AndreasOun <andreastthk@gmail.com>
2023-05-21 18:58:22 +0300

    Api fixes, CSS fixes

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,11 +10,22 @@ function Home() {
     async function fetchData() {
       const API_KEY = '8a4ddcf472e26bea20a3ea9f42810899';
       const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-      setMovies(response.data.results);
-      if (response.data.results.length > 0) {
-        setBackgroundImage(`https://image.tmdb.org/t/p/original/${response.data.results[0].backdrop_path}`);
+      const fetchedMovies = response.data.results;
+  
+      // Fetch additional pages of movies
+      for (let page = 2; page <= 50; page++) { 
+        const nextPageResponse = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`);
+        const nextPageMovies = nextPageResponse.data.results;
+        fetchedMovies.push(...nextPageMovies);
+      }
+  
+      setMovies(fetchedMovies);
+  
+      if (fetchedMovies.length > 0) {
+        setBackgroundImage(`https://image.tmdb.org/t/p/original/${fetchedMovies[0].backdrop_path}`);
       }
     }
+  
     fetchData();
   }, []);
 
@@ -24,18 +35,38 @@ function Home() {
         <h1>Welcome to Notlfix!</h1>
         <p>Here you can find your favorite movies and TV shows.</p>
       </div>
-      <ul className="movie-list">
-        {movies.map(movie => (
-          <li key={movie.id} className="movie-card">
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-            <div className="overlay">
-              <h2>{movie.title}</h2>
-              <p>{movie.overview}</p>
-              <button>Watch Now</button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="categories">
+        <h2>Action</h2>
+        <ul className="movie-list">
+          {movies
+            .filter((movie) => movie.genre_ids.includes(28))
+            .map((movie) => (
+              <li key={movie.id} className="movie-card">
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                <div className="overlay">
+                  <h2>{movie.title}</h2>
+                  <p>{movie.overview}</p>
+                  <button>Watch Now</button>
+                </div>
+              </li>
+            ))}
+        </ul>
+        <h2>Anime</h2>
+        <ul className="movie-list">
+          {movies
+            .filter((movie) => movie.genre_ids.includes(16))
+            .map((movie) => (
+              <li key={movie.id} className="movie-card">
+                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                <div className="overlay">
+                  <h2>{movie.title}</h2>
+                  <p>{movie.overview}</p>
+                  <button>Watch Now</button>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
